9452ec91f96789ac57c2582ced826825d9242ca3
Jonathan Afek <jonathan@mobb.dev>
2023-09-10 21:51:21 +0300

    fix

--- src/main/java/SQLInjectionExample.java
+++ src/main/java/SQLInjectionExample.java
@@ -1,10 +1,8 @@
 import java.sql.*;
 
 public class SQLInjectionExample {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args, Connection con) throws SQLException {
         String userInputA = args[1];
-
-        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db", "root", "root");
 
         String query = "SELECT * FROM users WHERE username = '" + userInputA + "';";
         Statement stmt = con.createStatement();
