44b6d03d0c4dd3c9b705c669413dbfa47e38db87
Eitan Worcel <96389636+mobbeitan@users.noreply.github.com>
2023-09-15 07:13:48 -0400

    Create without-logger.java

---
+++ src/main/java/without-logger.java
@@ -0,0 +1,35 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+
+public class SomeClass {
+
+    public void doSomething() {
+        try {
+            FileOutputStream fos = new FileOutputStream("fileout.txt");  
+            PrintStream ps = new PrintStream(fos);
+            FileWriter fw = new FileWriter("output.txt");
+            PrintWriter pw = new PrintWriter(fw, true);
+            try {
+                int a = 30, b = 0;
+                int c = a / b; // cannot divide by zero
+                System.out.println("Result = " + c);
+
+                int[] myNumbers = {1, 2, 3};
+                System.out.println(myNumbers[10]);
+
+            } catch (ArithmeticException e) {
+                e.printStackTrace();
+            } catch (ArrayIndexOutOfBoundsException ex) {
+                ex.printStackTrace();ex.printStackTrace(ps);ex.printStackTrace(pw);
+            }
+        } catch (IOException io) {      io.printStackTrace();}
+        catch (Exception e) {
+            // do nothing
+            e.printStackTrace();
+        }
+    }
+}
